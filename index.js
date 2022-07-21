b6b9261bdae5713aabeab85398304c8784cd3b98
Nosipho <manyasenosipho@gmail.com>
2022-07-21 11:17:50 +0200

    create and target grids

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as d3 from 'https://unpkg.com/d3?module'
+// import * as d3 from 'https://unpkg.com/d3?module'
 
 
 const quadrantOne = document.getElementById("quadrant-one")
@@ -21,8 +21,8 @@ fetch('https://tech-radar-api.herokuapp.com/tech-radar')
     .then(data => {
         sortData(data)
         // createTechBlips()
-        renderTechBlips()
-        generateCircles()
+            // renderTechBlips()
+            // generateCircles()
     })
 
 
@@ -42,127 +42,48 @@ function sortData(data){
             techniques.push(technology)
         }
     })
-}   
+} 
 
+function createGrids(){
 
-function rAndTheta(radius){
-    // console.log(Math.sqrt(Math.random()))
-    const r = radius  * Math.sqrt(Math.random())
-    const theta = Math.random() * 2 * Math.PI
-    return [r, theta]
-}
+    const column = 100
+    const row = 100
 
-    // r = R * sqrt(random())
-    // theta = random() * 2 * PI
-        
-    // x = 0 + r * cos(     theta)
-    // y = 0    + r * sin(theta)
+    let cellsArray = new Array(column)
 
-function posXandY(center, radius){
-    const r = rAndTheta(radius)[0] 
-    const theta = rAndTheta(radius)[1]
-
-    const x = center + r * Math.cos(theta)
-    const y = center + r * Math.sin(theta)
-
-
-    return [x,y]
-    
-}
-
-
-function generateCircles(){
-    
-    const gTag = d3.select('#adopt')
-        .attr('width', '250')
-        .attr('height', '250')
-        .append('g')
-        // .style('position', 'absolute')
-
-    const circle = gTag
-        .append('circle')
-        .attr('cx', 125)
-        .attr('cy', 125)
-        .attr("r", 120)
-        .attr("stroke", "black")
-        .attr("stroke-width", 3)
-        .attr("fill", "transparent")
-
-    // save pos. x and y to array
-
-    let xAndYarray = []
-    for(let i = 0; i<languagesAndFrameworks.length; i++){
-        let xAndY = posXandY(125,120)
-        xAndYarray.push(xAndY)
+    for(let i = 0; i<row; i++){
+        cellsArray[i] = new Array(row).fill(`<div class="grid-item">1</div>`)
     }
-    console.log(xAndYarray)
-    let countX = -1
-    let countY = -1
 
-    const techBlip = gTag
-        .selectAll('text')
-        .data(languagesAndFrameworks)
-        .enter().append('text')
-        .attr('x', () =>{
-            countX += 1
-            return  xAndYarray[countX][0]
-        } )
-        .attr('y', () =>{
-            countY += 1
-            return xAndYarray[countY][1]
-        })
-        .html((technology)=> technology.techPlaceholderNum)
-        
+    let cells1dArray = []
 
-    const circleTwo = d3.select('#trial')
-    .attr('width', '400')
-    .attr('height', '400')
-    .append('g')
-    .append('circle')
-    .attr('cx', "200")
-    .attr('cy', "200")
-    .attr("r", 180)
-    .attr("stroke", "black")
-    .attr("stroke-width", 3)
-    .attr("fill", "transparent")
-        
+    //convert nested arrays then array to string
+    cellsArray.forEach(innerArray => cells1dArray.push(`<div class="column">\n${innerArray.join('\n')}\n</div>`))
+    let cellsArrayHtml = cells1dArray.join('\n')
 
-    const circleThree = d3.select('#assess')
-    .attr('width', '600')
-    .attr('height', '600')
-    .append('g')
-    .append('circle')
-    .attr('cx', "300")
-    .attr('cy', "300")
-    .attr("r", 250)
-    .attr("stroke", "black")
-    .attr("stroke-width", 3)
-    .attr("fill", "transparent")
+    const grid = document.getElementById('grid')
+    grid.innerHTML = cellsArrayHtml
 
-    const circleFour = d3.select('#hold')
-    .attr('width', '637')
-    .attr('height', '637')
-    .append('g')
-    .append('circle')
-    .attr('cx', "318.5")
-    .attr('cy', "318.5")
-    .attr("r", 315)
-    .attr("stroke", "black")
-    .attr("stroke-width", 3)
-    .attr("fill", "transparent")
+    grid.style.gridTemplateRows =  `repeat(${row}, 1fr)`
+    grid.style.gridTemplateColumns =  `repeat(${column}, 1fr)`
 
+    const columnHtml = document.getElementsByClassName('column')
+
+
+    //target specific grids
+    const cell = columnHtml[10].getElementsByClassName('grid-item')
+    cell[5].style.backgroundColor = "pink"
 
 }
 
+createGrids()
 
+// function renderTechBlips(){
+//     const allHtml = createTechBlips()
 
-
-function renderTechBlips(){
-    // const allHtml = createTechBlips()
-
-    // quadrantOne.innerHTML += allHtml[0]
-    // quadrantTwo.innerHTML += allHtml[1]
-    // quadrantThree.innerHTML += allHtml[2]
-    // quadrantFour.innerHTML += allHtml[3]
+//     quadrantOne.innerHTML += allHtml[0]
+//     quadrantTwo.innerHTML += allHtml[1]
+//     quadrantThree.innerHTML += allHtml[2]
+//     quadrantFour.innerHTML += allHtml[3]
     
-}
+// }
