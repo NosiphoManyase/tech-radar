20ef66d69f2ee19db17b8801ab6b4173231d3b5a
Nosipho <manyasenosipho@gmail.com>
2022-07-29 13:46:32 +0200

    display tech names, refactor code

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ let tools = []
 let techniques = []
 let counter = 0
 let occupiedCells = []
-let column = 0
+let randomColumn = 0
 let randomRow = 0
-console.log(techniques)
+let column = 16
+let row = 16
+let isSingleQuadView = false
+// console.log(techniques)
+
 
 fetch('https://tech-radar-api.herokuapp.com/tech-radar')
     .then(response => response.json())
     .then(data => {
-        // console.log(data)
+        console.log(data)
         sortData(data) 
-        createGrids("platforms")
-        createGrids("langAndFrameworks")
-        createGrids("tools")
-        createGrids("techniques")
+        createGrids()
+        createGrids()
+        createGrids()
+        createGrids()
         sortIntoPhases(languagesAndFrameworks)  
         sortIntoPhases(platforms)
         sortIntoPhases(tools)
@@ -42,47 +46,46 @@ function sortData(data){
     })
 } 
 
-function checkDuplicates(randomRow, column){
+function checkDuplicates(randomRow, randomColumn){
     
     if(occupiedCells.length === 0){
-        occupiedCells.push([randomRow, column])
+        occupiedCells.push([randomRow, randomColumn])
 
     }else{
         let el = occupiedCells.find(el => el[0] === randomRow)
 
         if(el){
-            if(el[1] === column){   
-                column = randomColumn()
-                checkDuplicates(randomRow, column)
+            if(el[1] === randomColumn){   
+                randomColumn = randomColumnCalc()
+                checkDuplicates(randomRow, randomColumn)
             }else{
-                occupiedCells.push([randomRow, column])
+                occupiedCells.push([randomRow, randomColumn])
             }
             
         }else{
-            occupiedCells.push([randomRow, column])
+            occupiedCells.push([randomRow, randomColumn])
         }
         
     }
 }
 
-function randomColumn( ){
+function randomColumnCalc(){
     const randomColumn = Math.floor(Math.random() * 16)
     return randomColumn
 }
 
 function displayDataPoints(divId, techPlaceholder, techName){
-    console.log(techPlaceholder)
     let row = ''
     row = document.getElementById(divId).getElementsByClassName(`row${randomRow}`)
     checkDuplicates(randomRow, column)
-    row[column].innerHTML += 
+    row[randomColumn].innerHTML += 
     `<span class='data-point'>${techPlaceholder}</span>
     <span class = 'data-name'>${techName}</span>`
 }
 
 function sortIntoQuadrants(tech){
 
-            column = randomColumn()
+            randomColumn = randomColumnCalc()
 
             if(tech.quadrant === 'languages and frameworks'){
 
@@ -136,28 +139,39 @@ function sortIntoPhases(quadrantData){
     
 }
 
-function createGrids(quadrantName){
+function mountGrids(quadrantName, cellsArrayHtml){
+    const quadrant = document.getElementById(`${quadrantName}`)
+    quadrant.innerHTML = cellsArrayHtml
+    quadrant.style.gridTemplateRows =  `repeat(${row}, 1fr)`
+    quadrant.style.gridTemplateColumns =  `repeat(${column},1fr)`
+}
 
-    const column = 16
-    const row = 16
+function createGrids(){
     let count = 0
     
     let cellsArray = new Array(column)
 
     for(let i = 0; i<row; i++){
         cellsArray[i] = new Array(row)
-        
-        if(i<4){
-            cellsArray[i].fill(`<div class="grid-item ${quadrantName} row${count+=1} adopt"></div>`)
-        }else if(i<8){
-            cellsArray[i].fill(`<div class="grid-item ${quadrantName} row${count+=1} trial"></div>`)
-        }else if(i<12){
-            cellsArray[i].fill(`<div class="grid-item ${quadrantName} row${count+=1} assess"></div>`)
+
+        let num = 0
+        if(!isSingleQuadView){
+            num = 4
         }else{
-            cellsArray[i].fill(`<div class="grid-item ${quadrantName} row${count+=1} hold"></div>`)
+            num = 15
+        }
+        
+        if(i<num){
+            cellsArray[i].fill(`<div class="grid-item row${count+=1} adopt"></div>`)
+        }else if(i<(num*2)){
+            cellsArray[i].fill(`<div class="grid-item row${count+=1} trial"></div>`)
+        }else if(i<(num*3)){
+            cellsArray[i].fill(`<div class="grid-item row${count+=1} assess"></div>`)
+        }else{
+            cellsArray[i].fill(`<div class="grid-item row${count+=1} hold"></div>`)
         }
     }
-
+    console.log(languagesAndFrameworks)
     let cells1dArray = []
     
     
@@ -165,31 +179,26 @@ function createGrids(quadrantName){
     cellsArray.forEach(innerArray => cells1dArray.push(`${innerArray.join('\n')}`))
     let cellsArrayHtml = cells1dArray.join('\n')
     
-
-    const langAndFrameworks = document.getElementById('langAndFrameworks')
-    langAndFrameworks.innerHTML = cellsArrayHtml
-    langAndFrameworks.style.gridTemplateRows =  `repeat(${row}, 1fr)`
-    langAndFrameworks.style.gridTemplateColumns =  `repeat(${column},1fr)`
-
-    if (quadrantName === 'platforms'){
-        const platformsQuad = document.getElementById('platforms')
-        platformsQuad.innerHTML = cellsArrayHtml
-        platformsQuad.style.gridTemplateRows =  `repeat(${row}, 1fr)`
-        platformsQuad.style.gridTemplateColumns =  `repeat(${column},1fr)`
-    }else if(quadrantName === 'tools'){
-        const toolsQuad = document.getElementById('tools')
-        toolsQuad.innerHTML = cellsArrayHtml
-        toolsQuad.style.gridTemplateRows =  `repeat(${row}, 1fr)`
-        toolsQuad.style.gridTemplateColumns =  `repeat(${column},1fr)`
-    }else{
-        const techniquesQuad = document.getElementById('techniques')
-        techniquesQuad.innerHTML = cellsArrayHtml
-        techniquesQuad.style.gridTemplateRows =  `repeat(${row}, 1fr)`
-        techniquesQuad.style.gridTemplateColumns =  `repeat(${column},1fr)`
-    }
-    
-
-
-    return cellsArray
+    mountGrids("langAndFrameworks", cellsArrayHtml)
+    mountGrids("platforms", cellsArrayHtml)
+    mountGrids("tools", cellsArrayHtml)
+    mountGrids("techniques", cellsArrayHtml)
+      
 }
 
+function update(){
+
+}
+
+function singleView(){
+    let singleQuad = document.getElementById('langAndFrameworks')
+
+    singleQuad.addEventListener('click', ()=> {
+        isSingleQuadView = true
+        column = 60
+        row = 60
+        createGrids()
+    })
+        
+    
+}
