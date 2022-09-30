37ce426b1f82f98d0d6cd10015711a6f11910f2c
Nosipho <manyasenosipho@gmail.com>
2022-09-30 12:15:48 +0200

    refactoring

--- languageAndFrameworks.js
+++ languageAndFrameworks.js
@@ -1,7 +1,18 @@
+let languagesData = []
 
 fetch("https://tech-radar-api.herokuapp.com/tech-radar")
   .then((response) => response.json())
   .then((data) => {
     console.log(data)
+    languagesData = data.filter( dataPoint => dataPoint.quadrant === "languages and frameworks")
+    assemble(languagesData)
     
-  });
+});
+
+function assemble(data){
+    const main = `<div> <p> Languages and Frameworks rendered</p></div>`
+
+    const renderpage = document.getElementById("langAndFrameworksQuad")
+    renderpage.innerHTML += main
+}
+
